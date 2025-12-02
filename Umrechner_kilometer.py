c61ff37a49196b6363a8c7695c517ca048232ab6
LStiefvater <leon.stiefvater@schwaemmle-ostfildern.de>
2025-12-02 14:21:31 +0000

    Füge Umrechner für Kilometer und Meter hinzu

---
+++ Umrechner_kilometer.py
@@ -0,0 +1,8 @@
+
+print("Willkommen beim KM umrechner!")
+meter = int (input("Gib deinen Meterwert ein:"))
+message = f"Deine Eingabe war: {meter} m"
+print(message)
+
+kilometer = meter / 1000
+print(kilometer)
---
+++ Umrechner_meter.py
@@ -0,0 +1,9 @@
+
+print("Willkommen beim M umrechner!")
+#Eingabe vom Kilometerstand 
+meter = int (input("Gib deinen Kilometerwert ein:"))
+message = f"Deine Eingabe war: {meter} km"
+print(message)
+
+kilometer = meter * 1000
+print(kilometer)
